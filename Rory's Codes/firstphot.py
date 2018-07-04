687ab2e72b26085d2dabbff221a78d76b20ebf60
Rory Lowe <ewolyror@gmail.com>
2018-07-04 14:30:10 -0600

    testing

--- Rory's Codes/firstphot.py
+++ Rory's Codes/firstphot.py
@@ -1,1 +1,4 @@
-"""firstphot.py a photometry program by rory lowe
+"""firstphot.py a photometry program by rory lowe as part of the gosnell research group attempting to use photutils to analyze HST data of NGC6819"""
+
+#import modules
+from photutils import make_source_mask
