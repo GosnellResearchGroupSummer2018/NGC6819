dbc3c544dc511eda0ab05ac8305a2560ee5150b7
Rory Lowe <ewolyror@gmail.com>
2018-07-06 11:34:06 -0600

    added more to my code files

--- Rory's Codes/samap.py
+++ Rory's Codes/samap.py
@@ -1,1 +1,8 @@
 """samap.py an aperture photometry program using sample images"""
+from photutils import make_source_mask
+from astropy.stats import sigma_clipped_stats
+from pylab import *
+from scipy import *
+from numpy import *
+from photutils import datasets
+id, xcentroid, ycentroid, sharpness, roundness1, roundness2, npix, sky, peak, flux, mag = load('samsdout.npy')
