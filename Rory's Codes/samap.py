70c65ace74c1d3628a78e964a5904fe0e91ae9ef
Rory Lowe <ewolyror@gmail.com>
2018-07-10 09:46:30 -0600

    updated rory codes

--- Rory's Codes/samap.py
+++ Rory's Codes/samap.py
@@ -1,8 +1,17 @@
 """samap.py an aperture photometry program using sample images"""
-from photutils import make_source_mask
-from astropy.stats import sigma_clipped_stats
 from pylab import *
 from scipy import *
 from numpy import *
 from photutils import datasets
-id, xcentroid, ycentroid, sharpness, roundness1, roundness2, npix, sky, peak, flux, mag = loadtxt('samsdout.txt', unpack=True)
+from photutils import CircularAperture
+from photutils import aperture_photometry
+from astropy.io import ascii
+id, xcentroid, ycentroid, sharpness, roundness1, roundness2, npix, sky, peak, flux, mag = loadtxt('samsdout.txt',skiprows=1, unpack=True)
+xpos= array(xcentroid)
+ypos= array(ycentroid)
+pos=column_stack((xpos,ypos))
+apertures = CircularAperture(pos, r=3.)
+data = datasets.make_100gaussians_image()
+phot_table = aperture_photometry(data, apertures)
+print(phot_table)
+ascii.write(phot_table, 'samapout.txt',overwrite=True)
