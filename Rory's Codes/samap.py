3bdd410eab9ffe62cad690c19da8f69a276ee029
Rory Lowe <ewolyror@gmail.com>
2018-07-10 11:49:17 -0600

    changed some codes, new images

--- Rory's Codes/samap.py
+++ Rory's Codes/samap.py
@@ -6,12 +6,18 @@ from photutils import datasets
 from photutils import CircularAperture
 from photutils import aperture_photometry
 from astropy.io import ascii
-id, xcentroid, ycentroid, sharpness, roundness1, roundness2, npix, sky, peak, flux, mag = loadtxt('samsdout.txt',skiprows=1, unpack=True)
+#import the data needed from source detection output
+xcentroid, ycentroid = loadtxt('samsdout.txt', usecols=(1,2), skiprows=1, unpack=True)
+#make arrays of x and y positions
 xpos= array(xcentroid)
 ypos= array(ycentroid)
+#turn those two arrays into a N*2 array
 pos=column_stack((xpos,ypos))
+#make an aperture for each source. r is the radius of the aperture
 apertures = CircularAperture(pos, r=3.)
-data = datasets.make_100gaussians_image()
+#define where to look for the counts inside each aperture
+data = load("data.npy")
+#perform aperture photometry on the data in the given apertures
 phot_table = aperture_photometry(data, apertures)
-print(phot_table)
+#save the resulting table to an output text file
 ascii.write(phot_table, 'samapout.txt',overwrite=True)
