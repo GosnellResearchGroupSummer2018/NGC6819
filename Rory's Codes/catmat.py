0b59c69938be458926799dfa9f49c4453770b8f4
Rory Lowe <ewolyror@gmail.com>
2018-08-14 09:39:33 -0600

    do i have anything to commit?

--- Rory's Codes/catmat.py
+++ Rory's Codes/catmat.py
@@ -1,1 +1,18 @@
 """catmat.py a program to match sources between different source catalogs"""
+#import modules
+from astropy.coordinates import SkyCoord
+from astropy import units as u
+from numpy import *
+#import position wcs data
+ibop04wcs = load("ibop04wcs.npy")
+#define first column as ra
+ra1 = ibop04wcs[:,0]
+#second column as dec
+dec1 = ibop04wcs[:,1]
+#do it with the other data
+ib2o01wcs = load("ib2o01wcs.npy")
+ra2 = ib2o01wcs[:,0]
+dec2 = ib2o01wcs[:,1]
+c = SkyCoord(ra=ra1*u.degree, dec=dec1*u.degree)
+catalog = SkyCoord(ra=ra2*u.degree, dec=dec2*u.degree)
+idx, d2d, d3d = c.match_to_catalog_sky(catalog)
